3683700cdbfcd5f3ea22bf54decfa2123a5ac86a
ali-b14 <alib714@gmail.com>
2024-03-02 17:29:16 -0500

    created a basic template of variables to test for the scientific method calc

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -3,7 +3,7 @@ package com.zipcodewilmington.scientificcalculator;
 /**
  * Created by leon on 2/9/18.
  */
-public class MainApplication { //my name is ali
+public class MainApplication {
     public static void main(String[] args) {
         Console.println("Welcome to my calculator!");
         String s = Console.getStringInput("Enter a string");
