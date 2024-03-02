80b1b9b2bfb529451aad47dbcef5d2b93310a3a7
Chaz-Adams <caadams1922@gmail.com>
2024-03-02 15:33:42 -0500

    updated my class

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,17 +1,111 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.Scanner;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
-    public static void main(String[] args) {
-        Console.println("Welcome to my calculator!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
-
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+    static Scientific scientific = new Scientific();
+    public static void main(String[] args)
+    {
+        Console.println("\t\tWelcome to my calculator!");
+        menu();
     }
+
+    public static void menu()
+    {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do
+        {
+            System.out.println("Menu: ");
+            System.out.println("1. Simple Calculations");
+            System.out.println("2. Scientific Calculations");
+            System.out.println("3. Exit");
+            System.out.println("Enter your choice: ");
+
+            choice = scanner.nextInt();
+
+            switch(choice)
+            {
+                case 1:
+                    simpleCalcMenu();
+                case 2:
+                    scientificCalculationsMenu();
+                case 3:
+                    //
+            }
+
+        }while(choice != 3);
+        scanner.close();
+    }
+
+    private static void scientificCalculationsMenu()
+    {
+
+    }
+
+    private static void simpleCalcMenu()
+    {
+        Scanner  scanner = new Scanner(System.in);
+        int choice;
+        do
+        {
+            System.out.println("Simple Calculations Menu: ");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Division");
+            System.out.println("4. Multiplication");
+            System.out.println("5. Square");
+            System.out.println("6. Square Root");
+            System.out.println("7. Exponentiation");
+            System.out.println("8. Inverse");
+            System.out.println("9. Invert Sign");
+            System.out.println("10. Go Back");
+            System.out.println("Enter your choice: ");
+
+            choice = scanner.nextInt();
+
+            switch(choice)
+            {
+                case 1:
+                    // addMethod() //use scannerMethod()for each input
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }while(choice != 10);
+        scanner.close();
+
+    }
+
+    public static double scannerMethod()
+    {
+        System.out.println("Enter a value: ");
+        Scanner scanner = new Scanner(System.in);
+        double value = scanner.nextDouble();
+        return value;
+    }
+    
+
 }
---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Scientific.java
@@ -0,0 +1,249 @@
+package com.zipcodewilmington.scientificcalculator;
+import java.util.Scanner;
+
+public class Scientific {
+
+    private String mode = "";
+    private double userInput = scannerMethod();  /// Not sure how to have a scanner obj for all methods yet
+
+    private double memory = 0; //Will store whatever value
+    // To clear memory it will set the value to null or zero?? maybe
+
+    /*
+    * MAYBE THE MAIN SHOULD HAVE A MENU SAYING DO YOU WANT TO DO ADDITION, SUBTRACTION ETC.
+    * OR PRESS 99 TO ENTER SCIENTIFIC MODE
+    *
+    * THEN HAVE A MENU THAT SAYS DO YOU WANT TO DO TRIG FUNCTIONS, CONVERSIONS, LOG FUNCTIONS ETC.
+    *
+    * MAKE SURE INSIDE THE TRIG FUNCTIONS MENU I SET UP THE **SWITCH TRIG UNITS MODE(DEGREES, RADIANS)**
+    *
+    * DON'T FORGET TO ADD **AT LEAST** TWO OF MY OWN FEATURES
+    *
+    * */
+
+    public void selectOperationMenu()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\t---SCIENTIFIC OPERATIONS---\n");
+        System.out.println("--Select your choice by selecting number choice--");
+        System.out.println("\t1. TRIG FUNCTIONS");
+        System.out.println("\t2. CONVERSIONS");
+        System.out.println("\t3. LOGARITHMIC FUNCTIONS");
+        System.out.println("\t4. FACTORIAL FUNCTIONS");
+        System.out.println("\t5. EXIT SCIENTIFIC MODE");
+
+        int choice = scanner.nextInt();
+        runScientificCalc(choice);
+
+    }
+
+    private void runScientificCalc(int choice)
+    {
+        switch(choice){
+            case 1:
+                // Run trig switch or if else
+            case 2:
+                switchDisplayMode();
+            case 3:
+                runLogs();
+            case 4:
+                runFactorials();
+            case 5:
+                // I think the main has to have a main object and I can call the main.main()
+        }
+    }
+
+    private void runFactorials()
+    {
+    } // Not coded yet
+
+    private void runLogs()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\t---SCIENTIFIC LOGARITHM OPERATIONS---\n");
+        System.out.println("--Select your choice by selecting number choice--");
+        System.out.println("\t1. Log");
+        System.out.println("\t2. Inverse Logarithm");
+        System.out.println("\t3. Natural Logarithm");
+        System.out.println("\t4. Inverse Natural Logarithm");
+
+        int choice = scanner.nextInt();
+        if(choice == 1){
+            System.out.println(logarithm(this.userInput));
+        }else if (choice == 2){
+            System.out.println(inverseLogarithm(this.userInput));
+        }else if (choice == 3){
+            System.out.println(naturalLogarithm(this.userInput));
+        }else if (choice == 4){
+            System.out.println(inverseNaturalLogarithm(this.userInput));
+        }
+    }
+
+    public void switchDisplayMode()
+    {
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println("Make your choice!");
+        System.out.println("Enter '1': BINARY");
+        System.out.println("Enter '2': OCTAL");
+        System.out.println("Enter '3': DECIMAL");
+        System.out.println("Enter '4': HEXADECIMAL");
+
+        int numberChoice = scanner.nextInt();
+        switch(numberChoice){
+            case 1:
+                this.mode = "Binary";
+                break;
+                // Binary system: where there are only two possible values for each digit, zero or one
+            case 2:
+                this.mode = "Octal";
+                break;
+                // Octal system: the value of each place is based on the powers of 8
+            case 3:
+                this.mode = "Decimal";
+                break;
+                // Decimal system: based on base 10 and the place values are based on the powers of 10
+            case 4:
+                this.mode = "Hexadecimal";
+                break;
+                // Hexadecimal system: the value of each place is based on the powers of 16
+        }
+        System.out.println("Display Mode Switching To: " + mode);
+        scanner.close();
+        switchDisplayMode(mode);
+    }
+
+    public void switchDisplayMode(String inMode)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int input = scanner.nextInt();
+
+        if(inMode.equals("Binary"))
+        {
+            convertToBinary();
+        } else if (inMode.equals("Octal"))
+        {
+            convertToOctal();
+        } else if (inMode.equals("Hexadecimal"))
+        {
+            convertToHexadecimal();
+        } else if (inMode.equals("Decimal"))
+        {
+            convertToDecimal();
+        }
+        scanner.close();
+    } // I might need methods for all these so i can have proper inputs to get code to work
+
+    /*******************************Scientific Features Follows************************************** */
+
+    public String convertToBinary(int userInput)
+    {
+        return Integer.toBinaryString(userInput);
+    }
+
+    public String convertToOctal(int userInput)
+    {
+        return Integer.toOctalString(userInput);
+    }
+
+    public String convertToHexadecimal(int userInput)
+    {
+        return Integer.toHexString(userInput);
+    }
+
+    public String convertToDecimal(int userInput)
+    {
+        return Integer.toString(userInput);
+    }
+
+    public double sineValue(double userInput)
+    {
+        return Math.sin(userInput);
+    }
+
+    public double cosineValue(double userInput)
+    {
+        userInput = Math.toRadians(userInput);
+        return Math.cos(userInput);
+    }
+
+    public double tangentValue(double userInput)
+    {
+        userInput = Math.toRadians(userInput);
+        return Math.tan(userInput);
+    }
+
+    public double inverseSineValue(double userInput)
+    {
+        userInput = Math.toRadians(userInput);
+        return Math.asin(userInput);
+    }
+
+    public double inverseCosineValue(double userInput)
+    {
+        userInput = Math.toRadians(userInput);
+        return Math.acos(userInput);
+    }
+
+    public double inverseTangentValue(double userInput)
+    {
+        userInput = Math.toRadians(userInput);
+        return Math.atan(userInput);
+    }
+
+    public double logarithm(double userInput)
+    {
+        return Math.log10(userInput);
+    } // TEST ALL LOG STUFF
+
+    public double inverseLogarithm(double userInput)
+    {
+        return Math.pow(10,userInput);
+    } // TEST ALL LOG STUFF
+
+    public double naturalLogarithm(double userInput)
+    {
+        return Math.log(userInput);
+    }
+
+    public double inverseNaturalLogarithm(double userInput)
+    {
+        return Math.exp(userInput);
+    }
+
+    public long factorial(int userInput)
+    {
+        long temp = 1;
+        for (int i = 2; i < userInput; i++){
+            temp *= i;
+        }
+        return temp;
+    }
+
+    public void switchUnitsMode()
+    {} //Not coded yet
+
+    public void switchUnitsMode(String mode)
+    {} // Not coded yet
+
+    public static double scannerMethod()
+    {
+        System.out.println("Enter a value: ");
+        Scanner scanner = new Scanner(System.in);
+        double value = scanner.nextDouble();
+        return value;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
