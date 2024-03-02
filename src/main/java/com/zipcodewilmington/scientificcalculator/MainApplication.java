a93a193dab33486fe959d9a33e693839d00b655a
Szeng89 <shijinz898@gmail.com>
2024-03-02 18:34:47 -0500

    I add changes

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -7,13 +7,14 @@ import java.util.Scanner;
 public class MainApplication {
     public static void main(String[] args) {
         // commenting thse out for now
-//        Console.println("Welcome to my calculator!");
+       Console.println("Welcome to my calculator!");
 //        String s = Console.getStringInput("Enter a string");
 //        Integer i = Console.getIntegerInput("Enter an integer");
 //        Double d = Console.getDoubleInput("Enter a double.");
 //
 //        Console.println("The user input %s as a string", s);
-//        Console.println("The user input %s as a integer", i);
+//        Console.println("The user input %s a3
+//        2s a integer", i);
 //        Console.println("The user input %s as a d", d);
         Scanner scanner = new Scanner(System.in);
 
@@ -22,10 +23,11 @@ public class MainApplication {
         // Perform calculation
         calculator.askOperator(scanner);
 
-        System.out.println("Add: " + calculator.getAdd());
-        System.out.println("Subtract: " + calculator.getSubtract());
-        System.out.println("Multiplication: " + calculator.getMultiplication());
-        System.out.println("Division: " + calculator.getDivision());
+//        System.out.println("Add: " + calculator.getAdd());
+//        System.out.println("Subtract: " + calculator.getSubtract());
+//        System.out.println("Multiplication: " + calculator.getMultiplication());
+//        System.out.println("Division: " + calculator.getDivision());
+        System.out.println(calculator.getResult());
 
 
 
