cbe147931107da006668f1a19cc61db676efd49c
asanahmadli <asenahmedli@gmail.com>
2024-03-02 21:00:56 -0500

    I made last changes

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -4,21 +4,35 @@ package com.zipcodewilmington.scientificcalculator;
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
+
     public static void main(String[] args) {
         Console.println("Welcome to my calculator!");
         Console.defaultValue();
-        String s = Console.getStringInput("Enter a string!");
-        Integer i = Console.getIntegerInput("Enter an integer!");
-        Double d = Console.getDoubleInput("Enter a double!");
+//        String s = Console.getStringInput("Enter a string!");
+//       // Integer i = Console.getIntegerInput("Enter an integer!");
+//        Double d = Console.getDoubleInput("Enter a double!");
+//
+//
+//        Console.println("The user input %s as a string", s);
+//        //Console.println("The user input %s as a integer", i);
+//        Console.println("The user input %s as a double", d);
+//
+//
+//
+////        SimpleCalculator simpleCalculation = new SimpleCalculator();
+////        double dd = simpleCalculation.calculator('*',i,d);
+//
+//
+//
+//        System.out.println("Result  = "+ScientificCalculator.cosine(d));
 
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a double", d);
+StartCalculatorWithType.start();
+SimpleCalculator a = new SimpleCalculator();
 
-        SimpleCalculator simpleCalculation = new SimpleCalculator();
-        double dd = simpleCalculation.calculator('*',11,10);
 
-        System.out.println("Result  = "+dd);
+
+
+
 
     }
 }
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-  //%%%%%%%%%%%%%%///
+
 /**
  * Created by leon on 2/9/18.
  */
