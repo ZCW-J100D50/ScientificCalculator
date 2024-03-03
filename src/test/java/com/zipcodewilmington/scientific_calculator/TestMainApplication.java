472a95b7cdb4f44279cbcb00ad8c55ce2448f6ec
Danny Cao <dannycao1997@yahoo.com>
2024-03-02 19:06:52 -0500

    testing

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,10 +1,29 @@
 package com.zipcodewilmington.scientific_calculator;
 
+
+import com.zipcodewilmington.scientificcalculator.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by leon on 2/9/18.
  */
-public class TestMainApplication {
 
-    //khghghghghghghg
+public class TestMainApplication {
+    Calculator calc = new Calculator();
+    @Test //Test Comments
+
+    public void addTest () {
+        double result  = calc.add(12);
+                result = calc.add(13);
+        assertEquals(25,result,0.01);
+
+
+    }
+
+
+
+
 
 }
