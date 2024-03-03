056b2638bed2d9ce0dfc208d1dbc275fffc36bc9
Danny Cao <dannycao1997@yahoo.com>
2024-03-03 15:25:24 -0500

    recent changes

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -47,7 +47,7 @@ public class MainApplication {
                     break;
                 case 2:
                     // Change current value
-                    double newValue = Console.getDoubleInput("Please enter new value: ");
+                    doublegi newValue = Console.getDoubleInput("Please enter new value: ");
                     calculator.setCurrentValue(newValue);
                     break;
                 case 3:
